e558d579cabe670408342039c6281f168f9e1b4a
pado232 <ehsilver98@naver.com>
2024-12-09 18:34:47 +0900

    PrimaryButton 생성
    - 공통으로 사용될 버튼 생성
    - 뉴모피즘 디자인으로 그림자를 이용해 입체감 부여
    - ver으로 색상 변경 가능 (기본값 blue)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+
+import styled from "styled-components";
+
+import Lobby from "./pages/Lobby";
+
+const Background = styled.div`
+  color: #2b2b2b;
+  background-color: #e5e5e5;
+  min-height: 100vh;
+
+  @media (max-width: 480px) {
+    font-size: 0.7rem;
+  }
+`;
 
 function App() {
-  return <div className="App">프론트 브랜치 생성 확인하기</div>;
+  return (
+    <Background>
+      <Lobby />
+    </Background>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Lobby.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PrimaryButton from "../components/common/Buttons/PrimaryButton";
+
+const Lobby = () => {
+  return <PrimaryButton ver="red">친구랑 하기</PrimaryButton>;
+};
+
+export default Lobby;
