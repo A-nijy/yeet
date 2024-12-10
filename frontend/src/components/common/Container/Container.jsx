b2a7806cdf9c82642d7b5ca923d67b7362fd78d5
pado232 <ehsilver98@naver.com>
2024-12-11 01:44:22 +0900

    fix: Container 스타일링 및 적용을 위한 수정
    - 로비와 게임 실행 화면에서 구조적으로 겹치는 스타일링 분리 및 제거

--- frontend/src/components/common/Container/Container.jsx
+++ frontend/src/components/common/Container/Container.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
-  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
   max-width: 1200px;
-  padding: 1rem;
+  margin: 0 auto;
 `;
 
 const Container = ({ children }) => {
