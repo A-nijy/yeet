8bda3a4f94998cc38498c73c5ec07d01ce58d7ce
A-nijy <rainick98@gmail.com>
2024-12-16 16:20:32 +0900

    chore: 게임 결과 응답부분 재수정
    - 플레이어 리스트와 플레이어 총 점수 분리하여 각 Map으로 응답

--- backend/src/main/java/yeet/backend/dto/responseDto/GameDoneResponseDto.java
+++ backend/src/main/java/yeet/backend/dto/responseDto/GameDoneResponseDto.java
@@ -17,16 +17,20 @@ import java.util.Map;
 public class GameDoneResponseDto {
 
     private ResponseType type = ResponseType.GAME_DONE;
+    private Map<String, String> playerList = new HashMap<>();
     private Map<String, Integer> result = new HashMap<>();
     private String win;
 
     public GameDoneResponseDto(GameData gameData){
+
 
         String player1 = gameData.getPlayers().get(0);
         String player2 = gameData.getPlayers().get(1);
         Integer playerScore1 = gameData.getScoreBoard(player1).getScore("total");
         Integer playerScore2 = gameData.getScoreBoard(player2).getScore("total");
 
+        playerList.put("player1", player1);
+        playerList.put("player2", player2);
         result.put(player1, playerScore1);
         result.put(player2, playerScore2);
 
